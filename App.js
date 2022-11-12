d5524b228f5f4d53ccff35137d72c17e8201b358
RadVonSwag <drewstes1@gmail.com>
2022-11-12 15:44:50 -0600

    Setup screen-nav and base homescreen

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+
+const NavStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NavStack.Navigator>
+        <NavStack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: 'What Do You Want to Eat Today?' }}
+          />
+      </NavStack.Navigator>
+    </NavigationContainer>
   );
 }
 
