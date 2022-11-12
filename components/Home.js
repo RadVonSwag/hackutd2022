72cb2d92c36a57d077c68043bf394441faeeb1df
SomethingRandom <shishkebab259@gmail.com>
2022-11-12 17:02:05 -0600

    Home: Text is now centered

--- components/Home.js
+++ components/Home.js
@@ -4,10 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const Home = () => {
    return ( 
-    <View>
-        <Text>What do you want to eat today?</Text>
+    <View style={styles.container}>
+        <Text style={styles.centerText}>What Do {'\n'} You Want To {'\n'} Eat Today?</Text>
     </View>
    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    centerText: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    }
+  });
 
 export default Home;
